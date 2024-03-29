5ea2e58a7ce42099d0b3d45984fc23e82314d3c4
eka <21106050023@student.uin-suka.ac.id>
2024-03-29 14:54:16 +0700

    refactor: api service hide github token

--- app/src/main/java/com/seoeka/githubuser/data/retrofit/ApiService.kt
+++ app/src/main/java/com/seoeka/githubuser/data/retrofit/ApiService.kt
@@ -8,24 +8,24 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token YOU_PERSONAL_GITHUB_TOKEN")
     fun getSearch(
         @Query("q") username: String
     ): Call<GithubResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token YOU_PERSONAL_GITHUB_TOKEN")
     fun getDetailUser(
         @Path("username") username : String
     ): Call<DetailUserResponse>
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token YOU_PERSONAL_GITHUB_TOKEN")
     fun getListFollower(
         @Path("username") username: String
     ): Call<List<UserItems>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token YOU_PERSONAL_GITHUB_TOKEN")
     fun getListFollowing(
         @Path("username") username: String
     ): Call<List<UserItems>>
